cd483e54d2e741020723c77c2dbaf54555341ad9
Carlos Roberto Faria da Silva <ocarlosroberto@gmail.com>
2021-04-20 21:04:09 -0300

    Day 5

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { ProductService } from "./product.service";
 import { IProduct } from "./products";
 
 @Component({
@@ -7,62 +9,57 @@ import { IProduct } from "./products";
     styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit, OnDestroy {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string = '';
+    sub!: Subscription;
 
-    private _listFilter: string='';
-    get listFilter(): string{
+    private _listFilter: string = '';
+    get listFilter(): string {
         return this._listFilter;
     }
-    set listFilter(value: string){
+    set listFilter(value: string) {
         this._listFilter = value;
         this.filteredProducts = this.performFilter(value);
     }
 
     filteredProducts: IProduct[] = [];
 
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2021",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-        }
-    ];
+    products: IProduct[] = [];
+
+    constructor(private productService: ProductService) {
+
+    }
 
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
 
-    ngOnInit(): void{
-        this._listFilter= 'cart';
+    ngOnInit(): void {
+        this.sub = this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err,
+        });
     }
 
-    performFilter(filterBy: string): IProduct[]{
+    ngOnDestroy(): void{
+        this.sub.unsubscribe();
+    }
+
+    performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
         return this.products.filter((product: IProduct) =>
             product.productName.toLocaleLowerCase().includes(filterBy)
         );
     }
 
-    onRatingClicked(message: string): void{
+    onRatingClicked(message: string): void {
         this.pageTitle = 'Product List ' + message;
     }
 
